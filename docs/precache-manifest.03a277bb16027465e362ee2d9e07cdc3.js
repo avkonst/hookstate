704215321f4c7a4ee9dd3dc686f15ba20bfba935
Andrey Konstantinov <avkonst@gmail.com>
2019-08-14 18:55:09 +1200

    First version for the documentation website

---
+++ docs/precache-manifest.03a277bb16027465e362ee2d9e07cdc3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de72076c413712343fd810a77359f471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002f8ed76197cea8c7ea",
+    "url": "/static/js/2.70aa14ed.chunk.js"
+  },
+  {
+    "revision": "5829f89577e4a076c8f6",
+    "url": "/static/js/main.48d4bced.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
