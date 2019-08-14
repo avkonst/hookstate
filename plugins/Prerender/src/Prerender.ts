89ae89bfd861bebdc1ef24a698b7747cfbb86d4d
Andrey Konstantinov <avkonst@gmail.com>
2019-08-15 11:55:13 +1200

    Added ScopedPrerender

--- plugins/Prerender/src/Prerender.ts
+++ plugins/Prerender/src/Prerender.ts
@@ -1,4 +1,5 @@
-import { StateLink, Prerender } from '@hookstate/core';
+import React from 'react';
+import { StateLink, Prerender, useStateLink } from '@hookstate/core';
 import isEqual from 'lodash.isequal';
 
 // tslint:disable-next-line: function-name
@@ -8,4 +9,12 @@ export function EqualsPrerender<S, E extends {}, R>(
         link.with(Prerender).extended.enablePrerender(isEqual);
         return transform(link, prev);
     }
+}
+
+export function ScopedPrerender<S, E extends {}>(props: {
+    state: StateLink<S, E>,
+    children: (state: StateLink<S, E>) => React.ReactElement
+}) {
+    const scoped = useStateLink(props.state);
+    return props.children(scoped);
 }
