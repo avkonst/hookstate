b3b2744f2c1da0de92a177990eaba1a88e0279c1
Andrey Konstantinov <avkonst@gmail.com>
2019-08-22 22:41:13 +1200

    Added more tests

--- src/UseStateLink.test.tsx
+++ src/UseStateLink.test.tsx
@@ -3,20 +3,23 @@ import { useStateLink } from './UseStateLink';
 
 import { renderHook, act } from '@testing-library/react-hooks';
 
-test('primitive: counter', async () => {
-    const { result } = renderHook(() => {
-        const l = useStateLink(0);
-        return {
-            statelink: l,
-            rendered: l.get()
-        }
-    });
+test('primitive: should rerender used', async () => {
+    const { result } = renderHook(() => useStateLink(0));
 
-    expect(result.current.rendered).toStrictEqual(0);
+    expect(result.current.get()).toStrictEqual(0);
     act(() => {
-        result.current.statelink.set(p => p + 1);
+        result.current.set(p => p + 1);
     });
-    expect(result.current.rendered).toStrictEqual(1);
+    expect(result.current.get()).toStrictEqual(1);
+});
+
+test('primitive: should not rerender unused', async () => {
+    const { result } = renderHook(() => useStateLink(0));
+
+    act(() => {
+        result.current.set(p => p + 1);
+    });
+    expect(result.current.get()).toStrictEqual(0);
 });
 
 test('primitive: counter', async () => {
