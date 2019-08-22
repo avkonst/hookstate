41e82b2dc919758fea686cfa04e699dc8f24d00e
Andrey Konstantinov <avkonst@gmail.com>
2019-08-23 01:21:17 +1200

    Improved example for Untracked.

--- examples/src/examples/plugin-untracked.tsx
+++ examples/src/examples/plugin-untracked.tsx
@@ -3,22 +3,37 @@ import { useStateLink } from '@hookstate/core';
 import { Untracked } from '@hookstate/untracked';
 
 export const ExampleComponent = () => {
-    const state = useStateLink({ renderCount: 0, updateCount: 0 })
+    const state = useStateLink({ renders: 0, counter1: 0, counter2: 0 })
         .with(Untracked); // enable the plugin
+
     // this one will not trigger rerender,
     // so it is OK to call within the render
-    Untracked(state.nested.renderCount).set(p => p + 1);
+    Untracked(state.nested.renders).set(p => p + 1);
 
     return <>
-        <p>Rendered: {state.get().renderCount} times<br/>
-        Updated: {state.get().updateCount} times</p>
         <p>
-            <button // this will cause rerendering as the entire state is set
+            {/* the following is tracked usage */}
+            Rendered: {state.nested.renders.get()} times<br/>
+            {/* the following is untracked usage */}
+            Counter 1: {Untracked(state.nested.counter1).get()}<br/>
+            {/* the following is tracked usage */}
+            Counter 2: {state.nested.counter2.get()}
+        </p>
+        <p>
+            <button
+                // this will cause rerendering,
+                // because renderCount and counter2 are used and tracked
+                // and this update is tracked
                 onClick={() => state.set(p => p)}
-            >Rerender view</button>
-            <button // this will not cause rerendering, but will update the state:
-                onClick={() => Untracked(state.nested.updateCount).set(p => p + 1)}
-            >Update counter (untracked)</button>
+            >Force entire state update</button>
+            <button
+                // this will not cause rerendering, but will update the state:
+                onClick={() => state.nested.counter1.set(p => p + 1)}
+            >Update counter 1 (untracked get)</button>
+            <button
+                // this will not cause rerendering, but will update the state:
+                onClick={() => Untracked(state.nested.counter2).set(p => p + 1)}
+            >Update counter 2 (untracked set)</button>
         </p>
     </>
 }
