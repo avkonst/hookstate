0cf9a9a04db90b1e55d55220851e4b5ab9d71f09
Andrey Konstantinov <avkonst@gmail.com>
2020-03-21 09:34:47 +1300

    removing storybook

--- hookstate.js.org/docs/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.(js|mdx)'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-docs',
-  ],
-};
