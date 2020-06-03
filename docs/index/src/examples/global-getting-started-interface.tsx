daec721eb3f430aa9cd31c1c8b5d1a4e339ceb70
Andrey Konstantinov <avkonst@gmail.com>
2020-06-03 20:23:03 +1200

    more annotation for the example about exported state

--- docs/index/src/examples/global-getting-started-interface.tsx
+++ docs/index/src/examples/global-getting-started-interface.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { createState, useState, self, State } from '@hookstate/core';
 
+// internal variables
 const globalState = createState(0);
-
 const wrapState = (s: State<number>) => ({
     get: () => s[self].value,
     increment: () => s[self].set(p => p + 1)
 })
 
-const accessGlobalState = () => globalState[self].map(wrapState)
-const useGlobalState = () => useState(globalState)[self].map(wrapState)
+// The following 2 functions can be exported now:
+export const accessGlobalState = () => globalState[self].map(wrapState)
+export const useGlobalState = () => useState(globalState)[self].map(wrapState)
 
+// And here is how it can be used outside of a component ...
 setInterval(() => accessGlobalState().increment(), 3000)
-
+// ... and inside of a component
 export const ExampleComponent = () => {
     const state = useGlobalState();
     return <p>
