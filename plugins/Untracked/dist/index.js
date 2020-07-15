e863788ad075de2bc51bd94cdcfe5ac3cc295ca8
Andrey Konstantinov <avkonst@gmail.com>
2020-07-15 14:45:36 +1200

    3.0.0

--- plugins/Untracked/dist/index.js
+++ plugins/Untracked/dist/index.js
@@ -2,13 +2,11 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var core = require('@hookstate/core');
-
 var PluginID = Symbol('Untracked');
 function Untracked($this) {
     if ($this) {
         var th = $this;
-        var _a = th[core.self].attach(PluginID), _1 = _a[0], controls_1 = _a[1];
+        var _a = th.attach(PluginID), _1 = _a[0], controls_1 = _a[1];
         return {
             get: function () { return controls_1.getUntracked(); },
             set: function (v) { return controls_1.setUntracked(v); },
