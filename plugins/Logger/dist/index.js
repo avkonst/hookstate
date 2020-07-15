46130031a2d21535eabd4e477003a651c0acacf5
Andrey Konstantinov <avkonst@gmail.com>
2020-07-15 19:37:43 +1200

    migrate to release 3

--- plugins/Logger/dist/index.js
+++ plugins/Logger/dist/index.js
@@ -2,8 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var core = require('@hookstate/core');
-
 var LoggerPluginInstance = /** @class */ (function () {
     function LoggerPluginInstance() {
     }
@@ -34,15 +32,15 @@ var PluginID = Symbol('Logger');
 function Logger($this) {
     if ($this) {
         var th_1 = $this;
-        var _a = th_1[core.self].attach(PluginID), instance = _a[0], controls_1 = _a[1];
+        var _a = th_1.attach(PluginID), instance = _a[0], controls_1 = _a[1];
         if (instance instanceof Error) {
             // auto attach instead of throwing
             Logger(th_1);
-            instance = th_1[core.self].attach(PluginID)[0];
+            instance = th_1.attach(PluginID)[0];
         }
         var inst_1 = instance;
         return {
-            log: function () { return inst_1.log(th_1[core.self].path, controls_1); }
+            log: function () { return inst_1.log(th_1.path, controls_1); }
         };
     }
     return {
