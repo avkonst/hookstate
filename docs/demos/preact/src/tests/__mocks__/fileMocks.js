c9a2084e8b10d71e5494da63fc9e42a3f8456490
Andrey Konstantinov <avkonst@gmail.com>
2022-03-19 12:13:07 +1300

    delete unused packages, remap hookstate core via file link

--- docs/demos/preact/src/tests/__mocks__/fileMocks.js
+++
@@ -1,3 +0,0 @@
-// This fixed an error related to the CSS and loading gif breaking my Jest test
-// See https://facebook.github.io/jest/docs/en/webpack.html#handling-static-assets
-export default "test-file-stub";
