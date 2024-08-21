e0744fe7262372288c46834ab925fb403f796c44
LoganDark <git@logandark.mozmail.com>
2024-08-21 09:20:52 -0700

    add tests?

--- core/src/__tests__/Typing.tsx
+++ core/src/__tests__/Typing.tsx
@@ -8,7 +8,7 @@ test('check assignability on typescript level', async () => {
         let a = hookstate<{ a: string, b?: string }>({ a: 'a', b: 'b' })
         let b: State<{ a: string }> = a;
     }
-    
+
     {
         //assign state with extension method to a state without
         interface Inf {
@@ -20,5 +20,36 @@ test('check assignability on typescript level', async () => {
             })
         }))
         let b: State<{ a: string }> = a;
+    }
+
+    {
+        // array inference
+        let a = hookstate<{ a: string }[]>([]);
+        let b: State<{ a: string }> = a[0];
+    }
+
+    {
+        // nullable array inference
+        let a = hookstate<{ a: string }[] | null>(null);
+        let b: State<{ a: string }> | undefined = a[0];
+    }
+
+    {
+        // object inference
+        let a = hookstate<{ a: string }>({ a: 'a' });
+        let b: State<string> = a.a;
+    }
+
+    {
+        // nullable object inference
+        let a = hookstate<{ a: string } | null>(null);
+        let b: State<string> | undefined = a.a;
+    }
+
+    {
+        // null inference
+        let a = hookstate<null>(null);
+        // if this is not an error, inference works correctly
+        let b: 'a' extends keyof typeof a ? never : typeof a = a;
     }
 });
